d1c4b718558c21833dfb2ed2d579a08c91e4bc2e
SpiderUnderUrBed <spiderlord202@gmail.com>
2025-06-19 23:02:26 +1200

    Added database spec for postgres

--- src/pgdatabase.rs
+++ src/pgdatabase.rs
@@ -8,7 +8,7 @@ use sqlx::{Pool, Postgres as SqlxPostgres};
 // use sqlx::FromRow;
 
 use std::error::Error;
-mod databasespec;
+pub mod databasespec;
 pub use databasespec::{User, CreateUserData, RemoveUserData, UserDatabase};
 
 // } else if username == "testuser" {
@@ -29,7 +29,9 @@ impl Database {
             connection: connection.unwrap(),
         }
     }
-    pub async fn retrieve_user(&self, username: String) -> Option<User> {
+}
+impl UserDatabase for Database { 
+    async fn retrieve_user(&self, username: String) -> Option<User> {
         let enable_admin_user = std::env::var("ENABLE_ADMIN_USER").unwrap_or_default() == "true";
         let admin_user = std::env::var("ADMIN_USER").unwrap_or_default();
         let admin_password = std::env::var("ADMIN_PASSWORD").unwrap_or_default();
@@ -48,7 +50,7 @@ impl Database {
         }
     }
 
-    pub async fn fetch_all(&self, item: &str) -> Result<Vec<User>, sqlx::Error>{
+    async fn fetch_all(&self, item: &str) -> Result<Vec<User>, Box<dyn Error + Send + Sync>> {
         let users = sqlx::query_as::<_, User>(&format!("SELECT * FROM {}", item))
         .fetch_all(&self.connection)
         .await?;
@@ -56,7 +58,7 @@ impl Database {
         Ok(users)
     }
 
-    pub async fn get_from_database(&self, username: &str) -> Result<Option<User>, sqlx::Error>{ 
+    async fn get_from_database(&self, username: &str) -> Result<Option<User>, Box<dyn Error + Send + Sync>> { 
         let user = sqlx::query_as::<_, User>("SELECT * FROM users WHERE username = $1")
             .bind(username)
             .fetch_optional(&self.connection)
@@ -65,7 +67,7 @@ impl Database {
         Ok(user)
     }
 
-    pub async fn create_user_in_db(&self, user: CreateUserData) -> Result<User, sqlx::Error> {
+    async fn create_user_in_db(&self, user: CreateUserData) -> Result<User, Box<dyn Error + Send + Sync>> {
         let hashed = hash(&user.password, DEFAULT_COST).map_err(|e| {
             sqlx::Error::Protocol(e.to_string().into())
         })?;
@@ -82,7 +84,7 @@ impl Database {
     //     //DELETE FROM users WHERE username = $1
 
     // }
-    pub async fn remove_user_in_db(&self, user: RemoveUserData) -> Result<Option<User>, sqlx::Error> {
+    async fn remove_user_in_db(&self, user: RemoveUserData) -> Result<Option<User>, Box<dyn Error + Send + Sync>> {
         let user = sqlx::query_as::<_, User>("DELETE FROM users WHERE username = $1 RETURNING *")
             .bind(user.user)
         .fetch_optional(&self.connection)
