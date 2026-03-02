e7a59e74a109506889b7a988eebc9b03c7a8b49b
Louis Bailleau <louis13.bailleau@gmail.com>
2026-03-02 14:10:07 +0000

    fix: ensure k3d is accessible from the app container

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 
-# 2. Create a default cluster if it doesn't exist
+# Get the current container ID
+CONTAINER_ID=$(hostname)
+
+# Create a default cluster if it doesn't exist
 if ! k3d cluster get dev-cluster &> /dev/null; then
     k3d cluster create dev-cluster \
         --api-port 6443 \
@@ -9,6 +12,23 @@ if ! k3d cluster get dev-cluster &> /dev/null; then
         --wait
 fi
 
+# Join the app container to the k3d network
+# This allows 'app' to resolve 'k3d-dev-cluster-server-0'
+docker network connect k3d-dev-cluster $CONTAINER_ID || true
+
+mkdir -p ~/.kube
+# Point kubectl to the cluster
+# Because of 'network_mode: service:k3d', 0.0.0.0:6443 will actually work!
+k3d kubeconfig get dev-cluster > ~/.kube/config
+
+# Replace the host/port with the internal container name
+# We target the server-0 container directly on the internal port 6443
+sed -i "s|server: https://0.0.0.0:.*|server: https://k3d-dev-cluster-server-0:6443|g" ~/.kube/config
+
+# Trust the internal cert (which is usually issued for 127.0.0.1)
+kubectl config set-cluster k3d-dev-cluster --insecure-skip-tls-verify=true
+
+# Add completions
 k3d completion bash > ~/completion-for-k3d.bash
 echo 'source ~/completion-for-k3d.bash' >> ~/.bashrc
 
