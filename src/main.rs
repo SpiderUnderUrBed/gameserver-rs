71df2c193df8bce0a45172369c0f36b3fac211ac
SpiderUnderUrBed <SpiderUnderUrBed@proton.me>
2026-05-12 05:56:00 +1200

    Added user perms to oidc

--- src/main.rs
+++ src/main.rs
@@ -47,7 +47,6 @@ use axum_login::AuthManagerLayer;
 use axum_login::AuthUser;
 use axum_login::tower_sessions::{MemoryStore, SessionManagerLayer};
 use axum_login::{AuthManagerLayerBuilder, AuthnBackend};
-use axum_oidc::EmptyAdditionalClaims;
 use axum_oidc::OidcAuthLayer;
 use axum_oidc::OidcClaims;
 use axum_oidc::OidcClient;
@@ -468,6 +467,16 @@ struct SrcAndDest {
     dest: ApiCalls,
     metadata: String,
 }
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+struct OidcAdditionalClaims {
+    #[serde(default)]
+    user_perms: Option<Vec<UserPerm>>
+}
+
+impl axum_oidc::openidconnect::AdditionalClaims for OidcAdditionalClaims {}
+impl axum_oidc::AdditionalClaims for OidcAdditionalClaims {}
+
 
 // Some common api calls which is just what might get exchanged between the frontend and backend via api
 // this is needed rather than a bunch of structs or however else I might do it because in some cases I might not know what api call to expect
@@ -2323,7 +2332,7 @@ async fn routes_static(
     auth_layer: AuthManagerLayer<Backend, MemoryStore>,
 ) -> (
     Router<Arc<RwLock<AppState>>>,
-    Option<OidcAuthLayer<EmptyAdditionalClaims>>,
+    Option<OidcAuthLayer<OidcAdditionalClaims>>,
 ) {
     let base_path = std::env::var("SITE_URL")
         .map(|mut s| {
@@ -2367,14 +2376,14 @@ async fn routes_static(
 
     // OIDC layers and routes will not be constructed if there is an issue with creating the layer
     // and routes, and will just merge and empty router
-    let mut maybe_oidc_layer: Option<OidcAuthLayer<EmptyAdditionalClaims>> = None;
+    let mut maybe_oidc_layer: Option<OidcAuthLayer<OidcAdditionalClaims>> = None;
     let mut oidc_routes: Router<Arc<RwLock<AppState>>> = Router::new();
 
     if let Ok((raw_oidc_layer, _)) = get_oidc_layer().await {
         // adds the callback and the oidc route to actually start the login initiation (includes fallback for /oidc/ if the user adds a path, maybe not nessesary?)
         let callback_router: Router<Arc<RwLock<AppState>>> = Router::new().route(
             "/oidc/callback",
-            any(handle_oidc_redirect::<EmptyAdditionalClaims>),
+            any(handle_oidc_redirect::<OidcAdditionalClaims>),
         );
 
         let login_router: Router<Arc<RwLock<AppState>>> = Router::new()
@@ -2388,7 +2397,7 @@ async fn routes_static(
                         eprintln!("OIDC login layer error: {e:#?}");
                         e.into_response()
                     }))
-                    .layer(OidcLoginLayer::<EmptyAdditionalClaims>::new()),
+                    .layer(OidcLoginLayer::<OidcAdditionalClaims>::new()),
             );
 
         oidc_routes = Router::new().merge(callback_router).merge(login_router);
@@ -2420,8 +2429,8 @@ async fn routes_static(
 // client ids and secrets
 async fn get_oidc_layer() -> Result<
     (
-        OidcAuthLayer<EmptyAdditionalClaims>,
-        OidcClient<EmptyAdditionalClaims>,
+        OidcAuthLayer<OidcAdditionalClaims>,
+        OidcClient<OidcAdditionalClaims>,
     ),
     Box<dyn Error + Send + Sync>,
 > {
@@ -2442,7 +2451,7 @@ async fn get_oidc_layer() -> Result<
         get_env_var_or_arg("OIDC_SECRET", Some("axum-app-secret".into())).unwrap();
     let oidc_id: String = get_env_var_or_arg("OIDC_ID", Some("axum-app".into())).unwrap();
 
-    let client = OidcClient::<EmptyAdditionalClaims>::builder()
+    let client = OidcClient::<OidcAdditionalClaims>::builder()
         .with_default_http_client()
         .with_redirect_url(oidc_callback.parse()?)
         .with_client_id(ClientId::new(oidc_id))
@@ -2460,14 +2469,14 @@ async fn get_oidc_layer() -> Result<
 #[axum::debug_handler]
 async fn oidc_login_initiate(
     mut auth_session: AuthSession,
-    claims: Option<OidcClaims<EmptyAdditionalClaims>>,
+    claims: Option<OidcClaims<OidcAdditionalClaims>>,
 ) -> impl IntoResponse {
     if auth_session.user.is_some() {
         return Redirect::to("/").into_response();
     }
     if let Some(claims) = claims {
         let mut decoded_user = String::new();
-
+        let mut user_perms = Vec::new();
         // Code for decoding the claims itself
         // if let Ok(decoded_user_bytes) = STANDARD.decode(local_claim_name.to_string()){
         //     if let Ok(inner_decoded_user) = String::from_utf8(decoded_user_bytes){
@@ -2480,6 +2489,9 @@ async fn oidc_login_initiate(
         //     println!("C");
         //     return StatusCode::INTERNAL_SERVER_ERROR.into_response();
         // }
+        if let Some(user_perms_claim) = &claims.additional_claims().user_perms {
+            user_perms = user_perms_claim.to_vec();
+        }
 
         if let Some(claim_name) = claims.name() {
             if let Some(local_claim_name) = claim_name.get(None) {
@@ -2497,7 +2509,7 @@ async fn oidc_login_initiate(
         let user = User {
             username: decoded_user,
             password_hash: None,
-            user_perms: vec![],
+            user_perms,
         };
         match auth_session.login(&user).await {
             Ok(_) => eprintln!("login succeeded"),
