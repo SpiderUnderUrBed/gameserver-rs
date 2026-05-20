8b3ee6feadfc735f36c00463f3ac16e09b54c3e7
SpiderUnderUrBed <SpiderUnderUrBed@proton.me>
2026-05-20 18:19:37 +1200

    Fixed console spacing

--- src/frontend/src/components/dashboard/ConsolePanel.svelte
+++ src/frontend/src/components/dashboard/ConsolePanel.svelte
@@ -38,13 +38,11 @@
 </script>
 
 <main class="console-main w-full h-full flex flex-col">
-    <div class="mockup-code relative flex flex-col min-h-0 flex-1 px-2" id="consoleHistory">
+    <div class="bg-neutral rounded-box relative flex flex-col min-h-0 flex-1 p-2 font-mono text-sm" id="consoleHistory">
         <div onscroll={onScroll} bind:this={scrollContainer} class="overflow-y-auto overflow-x-hidden flex-1 min-h-0">
-			{#each serverConsole.consoleHistory as entry, i}
-				<pre bind:this={entries[i]} class={[{ 'text-info': entry.type !== 'input' }, "text-wrap wrap-break-word"]}>
-					<code>{entry.text}{entry.count > 0 ? ` (x${entry.count + 1})` : ''}</code>
-				</pre>
-			{/each}
+            {#each serverConsole.consoleHistory as entry, i}
+                <pre bind:this={entries[i]} class={[{ 'text-info': entry.type !== 'input' }, "text-wrap wrap-break-word m-0 p-0"]}><code>{entry.text}{entry.count > 0 ? ` (x${entry.count + 1})` : ''}</code></pre>
+            {/each}
         </div>
     </div>
 </main>
